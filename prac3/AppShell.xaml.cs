da0083cd16115054fe27ab21a050db555f9ab50d
isp41 <isp41@LAB17-11>
2024-09-11 16:08:40 +0300

    Добавьте файлы проекта.

---
+++ prac3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace prac3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ prac3/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace prac3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            if ((username == "Админ" || username == "Пользователь") && password == "123")
+            {
+                ElementsBlock.IsVisible = true;
+            }
+            else
+            {
+                DisplayAlert("Ошибка", "Неверное имя пользователя или пароль.", "OK");
+            }
+        }
+
+        private void OnCloseButtonClicked(object sender, EventArgs e)
+        {
+            Application.Current.Quit();
+        }
+    }
+
+}
